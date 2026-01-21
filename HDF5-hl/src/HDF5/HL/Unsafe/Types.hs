abaa7fe6680efdb17e961045c60322039dc8a3a2
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2026-01-21 18:59:04 +0300

    Use DerivingVia for types from fixed-vector

--- HDF5-hl/src/HDF5/HL/Unsafe/Types.hs
+++ HDF5-hl/src/HDF5/HL/Unsafe/Types.hs
@@ -401,36 +401,28 @@ instance Element a => Element (Complex a) where
     pokeH5        (castPtr ptr)   re
     pokeElemOffH5 (castPtr ptr) 1 im
 
-instance (Element a, F.Arity n) => Element (FB.Vec n a) where
-  typeH5       = Array (typeH5 @a) [F.length (undefined :: FB.Vec n a)]
-  fastSizeOfH5 = fastSizeOfH5 @a *  F.length (undefined :: FB.Vec n a)
+
+instance (Element a, F.Vector v a) => Element (F.ViaFixed v a) where
+  typeH5       = Array (typeH5 @a) [F.length (undefined :: F.ViaFixed v a)]
+  fastSizeOfH5 = fastSizeOfH5 @a *  F.length (undefined :: F.ViaFixed v a)
   alignmentH5  = alignmentH5  @a
   peekH5 ptr   = F.generateM (peekElemOffH5 (castPtr ptr))
   pokeH5 ptr v = F.imapM_ (pokeElemOffH5 (castPtr ptr)) v
-instance (Element a, F.Arity n) => Element (FV.Vec n a) where
-  typeH5       = Array (typeH5 @a) [F.length (undefined :: FV.Vec n a)]
-  fastSizeOfH5 = fastSizeOfH5 @a *  F.length (undefined :: FV.Vec n a)
-  alignmentH5  = alignmentH5  @a
-  peekH5 ptr   = F.generateM (peekElemOffH5 (castPtr ptr))
-  pokeH5 ptr v = F.imapM_ (pokeElemOffH5 (castPtr ptr)) v
-instance (Element a, F.Arity n, FU.Unbox n a) => Element (FU.Vec n a) where
-  typeH5       = Array (typeH5 @a) [F.length (undefined :: FU.Vec n a)]
-  fastSizeOfH5 = fastSizeOfH5 @a *  F.length (undefined :: FU.Vec n a)
-  alignmentH5  = alignmentH5  @a
-  peekH5 ptr   = F.generateM (peekElemOffH5 (castPtr ptr))
-  pokeH5 ptr v = F.imapM_ (pokeElemOffH5 (castPtr ptr)) v
-instance (Element a, F.Arity n, Storable a) => Element (FS.Vec n a) where
-  typeH5       = Array (typeH5 @a) [F.length (undefined :: FS.Vec n a)]
-  fastSizeOfH5 = fastSizeOfH5 @a *  F.length (undefined :: FS.Vec n a)
-  alignmentH5  = alignmentH5  @a
-  peekH5 ptr   = F.generateM (peekElemOffH5 (castPtr ptr))
-  pokeH5 ptr v = F.imapM_ (pokeElemOffH5 (castPtr ptr)) v
-instance (Element a, F.Arity n, FP.Prim a) => Element (FP.Vec n a) where
-  typeH5       = Array (typeH5 @a) [F.length (undefined :: FP.Vec n a)]
-  fastSizeOfH5 = fastSizeOfH5 @a *  F.length (undefined :: FP.Vec n a)
-  alignmentH5  = alignmentH5  @a
-  peekH5 ptr   = F.generateM (peekElemOffH5 (castPtr ptr))
-  pokeH5 ptr v = F.imapM_ (pokeElemOffH5 (castPtr ptr)) v
+  {-# INLINE fastSizeOfH5 #-}
+  {-# INLINE alignmentH5  #-}
+  {-# INLINE peekH5       #-}
+  {-# INLINE pokeH5       #-}
+
+deriving via (F.ViaFixed (FB.Vec n) a)
+    instance (Element a, F.Arity n) => Element (FB.Vec n a)
+deriving via (F.ViaFixed (FV.Vec n) a)
+    instance (Element a, F.Arity n) => Element (FV.Vec n a)
+deriving via (F.ViaFixed (FU.Vec n) a)
+    instance (Element a, F.Arity n, FU.Unbox n a) => Element (FU.Vec n a)
+deriving via (F.ViaFixed (FS.Vec n) a)
+    instance (Element a, F.Arity n, Storable a) => Element (FS.Vec n a)
+deriving via (F.ViaFixed (FP.Vec n) a)
+    instance (Element a, F.Arity n, FP.Prim a) => Element (FP.Vec n a)
 
 deriving newtype instance Element a => Element (Identity a)
 
